1d7dc5b34950f5f991b8374f4c7b42870eee6f36
Nasrat <Nasrat@gmail.com>
2025-07-08 12:50:56 +0500

    Fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "api";
+BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 
 const api = axios.create({
     baseURL: BASE_URL
