4ad9dfe7422ca4df8ac33f19778ba6f25609150c
Nasrat <Nasrat@gmail.com>
2025-07-08 12:05:04 +0500

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,35 @@ import express from 'express';
 import noteRoutes from './src/Routes/noteRoutes.js';
 import rateLimiter from './src/middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 
 
 // Create an Express application
 const app = express();
 
-// Middleware to parse JSON bodies
+// Middleware
 app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 //Routes
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: 'http://localhost:5173'
+    }));
+}
 
 app.use(rateLimiter);
 app.use('/api/notes', noteRoutes);
 
+//If it is in production mode
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
 
-
-//PORT
-const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => {
