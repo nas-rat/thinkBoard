1056bc8053a0c8e9a0d3efa9a04de8f8962b9c77
Nasrat <Nasrat@gmail.com>
2025-07-08 13:07:03 +0500

    RateLimit updated

--- backend/src/Config/upstash.js
+++ backend/src/Config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(10, "60 s"),
+    limiter: Ratelimit.slidingWindow(50, "60 s"),
 
 })
 
